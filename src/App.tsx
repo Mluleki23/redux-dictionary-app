3b9c7fbb9e2a64c8dfe7bacac21bb16344e038fc
Mluleki <melokuhlemluleki02@gmail.com>
2025-09-30 12:47:42 +0200

    feat: Implement dictionary functionality and add initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import { WordSearch } from "./component/WordSearch";
+import { DefinitionDisplay } from "./component/DefinitionDisplay";
 
 function App() {
-  
   return (
     <>
-      
+    <div className="app">
+      <h1>Dictionary</h1>
+      <WordSearch />
+      <hr />
+      <DefinitionDisplay />
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- store.ts
+++ store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import dictionaryReducer from './src/features/dictionary/dictionarySlice';
 export const store = configureStore({
   reducer: {
     // Reducers will be added here
+    dictionary: dictionaryReducer,
   },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
